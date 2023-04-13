fbe0cd135d7577c48976e8d02652d42cc117ab9b
Sourabh Mane <msourabh631@gmail.com>
2023-04-13 12:58:24 +0530

    first commit

---
+++ src/test/java/com/geekster/FetchingData2/FetchingData2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.FetchingData2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchingData2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
